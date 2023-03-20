f3a31b783ff0ff7cd62dfe1dfa9e6f3cc8d020c4
dansam <33277725+kwx4957@users.noreply.github.com>
2023-03-20 17:43:42 +0900

    init

---
+++ src/main/java/com/goorm/okim/OkimApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.okim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goorm/okim/OkimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.okim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
