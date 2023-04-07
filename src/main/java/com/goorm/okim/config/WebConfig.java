d65b0c1a04361177e0831735f926f2b259004a27
kmss6905 <kmss69052@naver.com>
2023-04-07 10:10:45 +0900

    cors allow origin all

--- src/main/java/com/goorm/okim/config/WebConfig.java
+++ src/main/java/com/goorm/okim/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.goorm.okim.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,24 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    public static final String HTTP_LOCALHOST_8080 = "http://localhost:8080";
-
-    @Value("${front.url.http}")
-    private String frontHttp;
-
-    @Value("${front.url.https}")
-    private String frontHttps ;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-
-                // (CORS) 아래의 주소에 대해서만 접근을 허용합니다.
-                .allowedOrigins(
-                        frontHttp,
-                        frontHttps,
-                        HTTP_LOCALHOST_8080
-                )
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowCredentials(false);
     }
